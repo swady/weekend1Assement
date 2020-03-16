c636cdb011ee373fe1d69c1ebf3005ded82152ea
Swadhin Kumar <31865102+swady@users.noreply.github.com>
2020-03-16 14:41:17 -0700

    updated

--- vowelorconsonent.py
+++ vowelorconsonent.py
@@ -4,3 +4,13 @@ if  x in y:
     print( x,'is vowel')
 else:
     print(x,'is consonent')
+
+    
+    
+# my changes
+x = raw_input('Enter an alphabet: ')
+if x in 'aeiou':
+    print(x+' is a vowel')
+else:
+    print(x+' not a vowel')
+    
